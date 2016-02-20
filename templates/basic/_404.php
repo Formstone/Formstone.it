f4c13705824d0bbf7e2e553d2ef34faf2bf6e0e7
Ben Plum <mr@benplum.com>
2016-02-20 11:34:38 -0500

    Style fixes

--- templates/basic/_404.php
+++ templates/basic/_404.php
@@ -1,1 +1,22 @@
-<h1>404 - Page Not Found</h1>
+<div class="typography">
+
+	<header class="page_header js-scroll_lock" data-scroll-offset="55">
+		<div class="fs-row js-scroll_contents">
+			<div class="fs-cell">
+				<h1 class="page_heading">404</h1>
+				<div class="page_intro">
+					<p>The requested page was not found.</p>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<div class="page_content">
+		<div class="fs-row">
+			<div class="fs-cell">
+				<? //$parsedown->text($page_content)?>
+			</div>
+		</div>
+	</div>
+
+</div>
